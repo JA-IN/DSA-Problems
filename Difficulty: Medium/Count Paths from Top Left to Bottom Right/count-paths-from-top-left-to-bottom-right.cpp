42dbf73e04a8abde84a2f77f535fde7a2a3d03c3
Harshil Jain <159618087+JA-IN@users.noreply.github.com>
2026-08-01 13:55:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Paths from Top Left to Bottom Right/count-paths-from-top-left-to-bottom-right.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int numberOfPaths(int m, int n) {
+        // code here
+         vector<vector<int>>matrix( m , vector<int>(n));
+
+        for(int j = 0 ; j < n ; j++){
+            matrix[m-1][j] = 1 ;
+        }
+        for(int i = 0 ; i< m ; i++){
+            matrix[i][n-1] = 1;
+
+        }
+
+        for(int i = m-2 ; i>=0 ; i--){
+            for(int j = n-2 ; j>=0 ; j--){
+                matrix[i][j] = matrix[i+1][j] + matrix[i][j+1];
+            }
+
+        }
+
+        return matrix[0][0];
+    }
+};
