c3ca6196ad2b9a67b9e577d182e2bf8c4cded240
Harshil Jain <159618087+JA-IN@users.noreply.github.com>
2026-08-08 13:41:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if Permutation is Substring/check-if-permutation-is-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+     bool sameFrequency(vector<int>& window, vector<int>& need) {
+        for (int i = 0; i < 26; i++) {
+            if (window[i] != need[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool search(string& txt, string& pat) {
+        // Write your code here
+         vector<int> need(26, 0);
+        vector<int> window(26, 0);
+
+        int n1 = pat.size(), n2 = txt.size();
+        if (n1 > n2) {
+            return false;
+        }
+        for (int i = 0; i < n1; i++) {
+            need[pat[i] - 'a']++;
+        }
+
+        for (int i = 0; i < n1; i++) {
+            window[txt[i] - 'a']++;
+        }
+
+        if (sameFrequency(window, need)) {
+            return true;
+        }
+        int right =
+            n1; // starting point of the where right should point after 1 window
+        for (int i = right; i < n2; i++) {
+            int remove = txt[i - n1] - 'a';
+            int add = txt[i] - 'a';
+
+            window[remove]--;
+            window[add]++;
+            if (sameFrequency(window, need)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
